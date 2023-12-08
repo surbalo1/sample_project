9899193918b932fb65a7d734dd2063978b6516a8
surbalo1 <54747170+surbalo1@users.noreply.github.com>
2023-12-08 15:20:57 -0600

    Update main.c

--- main/main.c
+++ main/main.c
@@ -1,4 +1,8 @@
-
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "driver/i2c.h"
+#include "ssd1306.h"
 #include "gpio_JRL/gpio.h"
 #include "gpio_JRL/gpio.c"
 
@@ -6,6 +10,10 @@
 #define RED PIN14
 #define GREEN PIN13
 #define BLUE PIN12
+
+#define SDA_PIN 21
+#define SCL_PIN 22
+
 void app_main(void)
 {
 //  Definir el número de pin GPIO que deseas configurar
@@ -55,6 +63,30 @@ void app_main(void)
   
     //int bit =  GPIO_IN_ADDR & (1 << 14)? 1: 0;
     //printf("%d", bit);
+
+    //FUNCION OLED
+    void OLED() {
+    i2c_config_t i2c_config = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = SDA_PIN,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_io_num = SCL_PIN,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = 100000
+    };
+    i2c_param_config(I2C_NUM_0, &i2c_config);
+    i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0);
+
+    ssd1306_set_i2c_address(0, 0x3C);
+    ssd1306_set_i2c_port(0);
+    ssd1306_set_screen(&screen);
+    ssd1306_clear_screen(&screen, false);
+    ssd1306_refresh(&screen, true);
+
+    // Agregar el mensaje de bienvenida en la pantalla OLED
+    ssd1306_write_string(&screen, 1, 1, "Hola, mundo!");
+    ssd1306_refresh(&screen, true);
+}
 
    
     return;
